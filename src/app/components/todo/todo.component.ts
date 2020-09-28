b608e61b874e2c8acfd56c12f0ba5f138ea298dc
galm85 <galm85@gmail.com>
2020-09-28 12:31:32 +0300

    craate to app in angular

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -9,7 +9,7 @@ import { Todo } from "../../interfaces/todo";
 export class TodoComponent implements OnInit {
 
   todos:Todo[]=[];
-
+  idNumber =1;
   constructor() { }
 
   renderTodos(){
@@ -17,7 +17,6 @@ export class TodoComponent implements OnInit {
       if(a.completed>b.completed) return 1;
       if(a.completed<b.completed) return -1;
       return 0;
-
     })
   }
 
@@ -25,8 +24,9 @@ export class TodoComponent implements OnInit {
     let list=localStorage.getItem('todoList')
     if(!list)return null;
     this.todos=JSON.parse(list);
+
   }
-  idNumber =1;
+
 
   addTodo(newTodo){
    let todo:Todo = {title:newTodo.value,completed:false,id:this.idNumber +1};
